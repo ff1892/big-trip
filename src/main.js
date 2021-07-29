42ed1030b60f6f3166752eea672493c063fc6dff
ff1892 <hitrulein@gmail.com>
2021-07-30 00:15:19 +0300

    Добавляет шаблоны компонентов

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import {createRouteTemplate} from './view/route';
+import {createTotalCostTemplate} from './view/total-cost';
+import {createMenuTemplate} from './view/menu';
+import {createFiltersTemplate} from './view/filters';
+import {createSortingTemplate} from './view/sorting';
+import {createEventsListTemplate} from './view/events-list';
+import {createEditPointTemplate} from './view/point-edit';
+import {createPointTemplate} from './view/point';
+import {createNewPointTemplate} from './view/point-new';
+
+const siteHeader = document.querySelector('.page-header');
+const mainTrip = siteHeader.querySelector('.trip-main');
+const tripControlsMenu = mainTrip.querySelector('.trip-controls__navigation');
+const tripControlsFilters = mainTrip.querySelector('.trip-controls__filters');
+
+const siteMain = document.querySelector('.page-main');
+const tripSorting = siteMain.querySelector('.trip-events');
+const tripEventsSection = siteMain.querySelector('.trip-events');
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+render(mainTrip, createRouteTemplate(), 'afterbegin');
+
+const tripInfo = mainTrip.querySelector('.trip-info');
+render(tripInfo, createTotalCostTemplate(), 'beforeend');
+
+render(tripControlsMenu, createMenuTemplate(), 'beforeend');
+render(tripControlsFilters, createFiltersTemplate(), 'beforeend');
+render(tripSorting, createSortingTemplate(), 'beforeend');
+
+render(tripEventsSection, createEventsListTemplate(), 'beforeend');
+
+const tripEventsList = tripEventsSection.querySelector('.trip-events__list');
+render(tripEventsList, createEditPointTemplate(), 'beforeend');
+render(tripEventsList, createPointTemplate(), 'beforeend');
+render(tripEventsList, createPointTemplate(), 'beforeend');
+render(tripEventsList, createNewPointTemplate(), 'beforeend');
+
+
