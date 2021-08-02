578e4d1a26fa45e2d213ff9f37fc8212fadb1dbf
ff1892 <hitrulein@gmail.com>
2021-08-02 17:09:47 +0300

    Генерирует моковые данные

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import {createRouteTemplate} from './view/route.js';
 import {createTotalCostTemplate} from './view/total-cost.js';
 import {createMenuTemplate} from './view/menu.js';
@@ -7,7 +8,10 @@ import {createEventsListTemplate} from './view/events-list.js';
 import {createEditPointTemplate} from './view/point-edit.js';
 import {createPointTemplate} from './view/point.js';
 import {createNewPointTemplate} from './view/point-new.js';
-import './mock/point.js';
+import {generateData} from './mock/point-data.js';
+
+const POINTS_COUNT = 10;
+const points = generateData(POINTS_COUNT);
 
 const siteHeader = document.querySelector('.page-header');
 const mainTrip = siteHeader.querySelector('.trip-main');
@@ -33,8 +37,14 @@ render(tripEventsSection, createEventsListTemplate(), 'beforeend');
 
 const tripEventsList = tripEventsSection.querySelector('.trip-events__list');
 render(tripEventsList, createEditPointTemplate(), 'beforeend');
-render(tripEventsList, createPointTemplate(), 'beforeend');
-render(tripEventsList, createPointTemplate(), 'beforeend');
+render(tripEventsList, createPointTemplate(points[0]), 'beforeend');
+render(tripEventsList, createPointTemplate(points[1]), 'beforeend');
 render(tripEventsList, createNewPointTemplate(), 'beforeend');
 
+// const a = points[0].dateFrom;
+// const b = points[0].dateTo;
 
+// console.log(dayjs(a).format('DD-MM-YY'))
+// console.log(dayjs(b).format('DD-MM-YY'))
+
+// const x = dayjs(b).diff(a, 'minutes');
---
+++ src/mock/point-data.js
@@ -0,0 +1,64 @@
+import dayjs from 'dayjs';
+import {getRandomInteger, getRandomArrayValue} from '../util.js';
+import {generateDestination} from './point-destination.js';
+import {EVENT_TYPES, getOffersForEvent} from './point-offer.js';
+
+const DESTINATIONS = ['Rome', 'Naples', 'Venice', 'Turin', 'Palermo', 'Florenze'];
+const MIN_PRICE = 0;
+const MAX_PRICE = 500;
+const DAYS_GAP = 5;
+const HOURS_GAP = 2;
+
+const getDestinationName = () => getRandomArrayValue(DESTINATIONS);
+const getEventType = () => getRandomArrayValue(EVENT_TYPES);
+
+const generateDate = () => {
+  let dateMin;
+  let dateMax;
+
+  const isSameDay = Boolean(getRandomInteger());
+
+  const getDaysDifference = () => getRandomInteger(-DAYS_GAP, DAYS_GAP);
+  const getHoursDifference = () => getRandomInteger(-HOURS_GAP, HOURS_GAP);
+
+  isSameDay ? (
+    dateMin = dayjs().add(getHoursDifference(), 'hours').toDate(),
+    dateMax = dayjs().add(getHoursDifference(), 'hours').toDate()
+  ) : (
+    dateMin = dayjs().add(getDaysDifference(), 'days').add(getHoursDifference(), 'hours').toDate(),
+    dateMax = dayjs().add(getDaysDifference(), 'days').add(getHoursDifference(), 'hours').toDate()
+  );
+
+  if (dateMin > dateMax) {
+    [dateMin, dateMax] = [dateMax, dateMin];
+  }
+
+    return {
+      min: dateMin,
+      max: dateMax,
+    }
+};
+
+
+const generatePoint = (id) => {
+  const destinationName = getDestinationName();
+  const type = getEventType();
+  const date = generateDate();
+
+  const point = {
+    id,
+    type,
+    name: destinationName,
+    price: getRandomInteger(MIN_PRICE, MAX_PRICE),
+    dateFrom: date.min,
+    dateTo: date.max,
+    isFavorite: Boolean(getRandomInteger()),
+    offers: getOffersForEvent(type),
+    destination: generateDestination(destinationName),
+  };
+  return point;
+};
+
+const generateData = (pointsCount) => new Array(pointsCount).fill().map((value, index) => generatePoint(index + 1));
+
+export {generateData};
